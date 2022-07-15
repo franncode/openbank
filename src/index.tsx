1c795a969c2dd71b132643d49c5d8719d09d955f
Francisco Rodriguez <fjrodriguez.353@gmail.com>
2022-07-15 13:11:58 -0300

    feat: routes updated

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,11 @@
 import './styles/global.scss'
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import {
+	BrowserRouter,
+	Routes,
+	Route,
+	Navigate,
+	Outlet,
+} from 'react-router-dom'
 import * as serviceWorker from './serviceWorker'
 import React from 'react'
 import ReactDOM from 'react-dom'
@@ -8,16 +14,23 @@ import { Home } from './components/pages/home/home'
 import { NotFound } from './components/pages/notFound/notFound'
 import { Two } from './components/pages/create/2/2'
 import { Three } from './components/pages/create/3/3'
+import { Locator } from './components/atoms/locator/locator'
 
 ReactDOM.render(
 	<BrowserRouter>
 		<Routes>
-			<Route path='/' element={<Home />} />
-			<Route path='/create/1' element={<One />} />
-			<Route path='/create/2' element={<Two />} />
-			<Route path='/create/3' element={<Three />} />
-			<Route path='*' element={<Navigate to='/not-found' />} />
-			<Route path='/not-found' element={<NotFound />} />
+			<Route index element={<Navigate to='/en' />} />
+			<Route path='/:language' element={<Locator />}>
+				<Route index element={<Home />} />
+				<Route path='create' element={<Outlet />}>
+					<Route index element={<Navigate to='1' />} />
+					<Route path='1' element={<One />} />
+					<Route path='2' element={<Two />} />
+					<Route path='3' element={<Three />} />
+					<Route path='not-found' element={<NotFound />} />
+				</Route>
+				<Route path='*' element={<Navigate to='/not-found' />} />
+			</Route>
 		</Routes>
 	</BrowserRouter>,
 	document.getElementById('root')
