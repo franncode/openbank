9cb39a888a53389ffc82099144308dbf3149906c
Francisco Rodriguez <fjrodriguez.353@gmail.com>
2022-07-18 04:58:56 -0300

    feat: update head tags

--- src/components/atoms/head/head.tsx
+++ src/components/atoms/head/head.tsx
@@ -2,6 +2,8 @@ import React, { FC } from 'react'
 import { Helmet } from 'react-helmet'
 import { THead } from './head.types'
 import openGraphPicture from '../../../assets/components/atoms/head/openGraphPicture.jpg'
+import { useTranslation } from 'react-i18next'
+import { theme } from '../../../styles/theme'
 
 export const Head: FC<THead> = ({
 	description = 'Almacene contraseñas, inicios de sesión y registros digitales en sus sitios favoritos. Mantenga sus inicios de sesión seguros.',
@@ -12,26 +14,34 @@ export const Head: FC<THead> = ({
 		picture: openGraphPicture,
 	},
 	title = 'Password Manager',
-}) => (
-	<Helmet>
-		<title>{title}</title>
-		<link rel='icon' href='/favicon.png' />
-		<meta name='viewport' content='initial-scale=1.0, width=device-width' />
-		<meta name='description' content={description} />
-		<meta name='theme-color' content='#FF0049' />
-		<meta name='msapplication-navbutton-color' content='#FF0049' />
-		<meta name='apple-mobile-web-app-status-bar-style' content='#FF0049' />
-		<meta itemProp='description' content={openGraph.description} />
-		<meta itemProp='image' content={openGraph.picture} />
-		<meta itemProp='name' content={title} />
-		<meta name='description' content={openGraph.description} />
-		<meta name='image' content={openGraph.picture} />
-		<meta name='og:description' content={openGraph.description} />
-		<meta name='og:image' content={openGraph.picture} />
-		<meta name='og:locale' content='es_ES' />
-		<meta name='og:site_name' content={title} />
-		<meta name='og:title' content={title} />
-		<meta name='og:type' content='website' />
-		<meta name='og:url' content={openGraph.link} />
-	</Helmet>
-)
+}) => {
+	const { i18n } = useTranslation()
+	const { language } = i18n
+
+	return (
+		<Helmet>
+			<title>{title}</title>
+			<meta name='description' content={description} />
+			<meta
+				name='msapplication-navbutton-color'
+				content={theme.color.primary}
+			/>
+			<meta
+				name='apple-mobile-web-app-status-bar-style'
+				content={theme.color.primary}
+			/>
+			<meta itemProp='description' content={openGraph.description} />
+			<meta itemProp='image' content={openGraph.picture} />
+			<meta itemProp='name' content={title} />
+			<meta name='description' content={openGraph.description} />
+			<meta name='image' content={openGraph.picture} />
+			<meta name='og:description' content={openGraph.description} />
+			<meta name='og:image' content={openGraph.picture} />
+			<meta name='og:locale' content={language} />
+			<meta name='og:site_name' content={title} />
+			<meta name='og:title' content={title} />
+			<meta name='og:type' content='website' />
+			<meta name='og:url' content={openGraph.link} />
+		</Helmet>
+	)
+}
