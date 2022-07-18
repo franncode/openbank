793fcec2cf599bf4a8cb882b67f157120d49efe1
Francisco Rodriguez <fjrodriguez.353@gmail.com>
2022-07-18 19:06:26 -0300

    fix: check function use

--- src/business/clue/clue.test.ts
+++ src/business/clue/clue.test.ts
@@ -6,11 +6,11 @@ describe('Business logic: Clue', () => {
 
 	test('Maximum 255 characters', () => {
 		expect(validClue.length).toBeLessThanOrEqual(255)
-		expect(clue.respect(validClue)).toEqual(validResponse)
+		expect(clue.check(validClue)).toEqual(validResponse)
 
 		const invalidClue =
 			'Ten en cuenta que toda inversión en mercados financieros está sujeta a fluctuaciones del mercado y a determinados riesgos, incluido el riesgo de pérdida del principal invertido y/o de ausencia de rentabilidad. Las rentabilidades pasadas no representan un indicador fiable de resultados futuros.'
-		expect(clue.respect(invalidClue)).toEqual({
+		expect(clue.check(invalidClue)).toEqual({
 			error: 'Maximum 255 characters',
 			ok: false,
 		})
